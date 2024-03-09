4b069235b5abe8087ea470b92cd97ee7f5d989f1
dkhaohao <ddddddkhaohao@gmail.com>
2024-03-09 17:24:27 +0800

    第一阶段:命令行输入参数生成代码

--- code-generator-basic/src/main/java/com/dkhaohao/Main.java
+++ code-generator-basic/src/main/java/com/dkhaohao/Main.java
@@ -1,5 +1,6 @@
 package com.dkhaohao;
 
+import com.dkhaohao.cli.command.CommandExecutor;
 import com.dkhaohao.generator.StaticGenerator;
 
 import java.io.File;
@@ -13,17 +14,9 @@ import java.io.File;
  */
 public class Main {
     public static void main(String[] args) {
-        //获取当前项目的跟目录
-        String projectPath=System.getProperty("user.dir");
-        System.out.println(projectPath);
-        File parentFile=new File(projectPath).getParentFile();
-        //输入路径：ACM 示例代码模版目录
-        String sourcePath =
-                new File(parentFile, "code-generator-demo-projects"+File.separator+"acm-first-template.ftl").getAbsolutePath();
-        //输出路径：直接输出到项目的根目录
-        String destPath=projectPath;
-        StaticGenerator.copyFilesByHutool(sourcePath,destPath);
-
+        args=new String[]{"list"};
+        CommandExecutor commandExecutor=new CommandExecutor();
+        commandExecutor.doExecute(args);
 
     }
 }
