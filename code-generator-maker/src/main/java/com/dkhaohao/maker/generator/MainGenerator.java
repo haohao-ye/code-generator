beafb960d5ba042a1f6cafa4f3f5469ec4d45679
dkhaohao <ddddddkhaohao@gmail.com>
2024-05-20 08:44:59 +0800

    从原始模版文件路径复制到生成的代码包中

--- code-generator-maker/src/main/java/com/dkhaohao/maker/generator/MainGenerator.java
+++ code-generator-maker/src/main/java/com/dkhaohao/maker/generator/MainGenerator.java
@@ -22,7 +22,6 @@ public class MainGenerator {
     public static void main(String[] args) throws TemplateException, IOException, InterruptedException {
         //获取Meta类
         Meta meta = MetaManager.getMeta();
-        System.out.println(meta);
 
         //输出路径
         String projectPath = System.getProperty("user.dir");
@@ -31,6 +30,11 @@ public class MainGenerator {
         if (!FileUtil.exist(destPath)) {
             FileUtil.mkdir(destPath);
         }
+
+        //从原始模版文件路径复制到生成的代码包中
+        String sourceRootPath=meta.getFileConfig().getSourceRootPath();
+        String sourceCopyDestPath=destPath+File.separator+".source";
+        FileUtil.copy(sourceRootPath,sourceCopyDestPath,false);
 
         //读取Resource目录
         ClassPathResource classPathResource = new ClassPathResource("");
@@ -94,6 +98,11 @@ public class MainGenerator {
         destModelPath = destPath + File.separator + "pom.xml";
         DynamicFileGenerator.doGenerator(sourceModelPath, destModelPath, meta);
 
+        //README.md
+        sourceModelPath = resourceAbsolutePath + File.separator + "templates/README.md.ftl";
+        destModelPath = destPath + File.separator + "README.md";
+        DynamicFileGenerator.doGenerator(sourceModelPath, destModelPath, meta);
+
         //构建jar包
         JarGenerator.doGenerate(destPath);
 
@@ -102,6 +111,20 @@ public class MainGenerator {
         String jarName=String.format("%s-%s-jar-with-dependencies.jar",meta.getName(),meta.getVersion());
         String jarPath="target/"+jarName;
         ScriptGenerator.doGenerator(shellDestPath,jarPath);
+
+        //精简版
+        String distDestPath=destPath+"-dist";
+        //拷贝jar包
+        String targetAbsolutePath=distDestPath+File.separator+"target";
+        FileUtil.mkdir(targetAbsolutePath);
+        String jarAbsolutePath = destPath + File.separator + jarPath;
+        FileUtil.copy(jarAbsolutePath,targetAbsolutePath,true);
+        ///拷贝脚本文件
+        FileUtil.copy(shellDestPath,distDestPath,true);
+        FileUtil.copy(shellDestPath+".bat",distDestPath,true);
+        //拷贝源文件
+        FileUtil.copy(sourceCopyDestPath,distDestPath,true);
+
 
 
     }
